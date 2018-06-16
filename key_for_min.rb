69d85593ce7743d2f3d0507472e81b67f735963c
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-16 12:32:50 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -15,3 +15,17 @@ def key_for_min_value(name_hash)
     end
   end
 end
+
+
+# Flatiron solution below...
+#def key_for_min_value(hash)
+#  lowest_key = nil
+#  lowest_value = nil
+#  hash.each do |k, v|
+#    if lowest_value == nil || v < lowest_value
+#      lowest_value = v
+#      lowest_key = k
+#    end
+#  end
+# lowest_key
+#end
